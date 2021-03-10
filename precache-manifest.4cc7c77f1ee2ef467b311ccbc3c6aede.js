f1396cf08d22f0a11257d4d4155767c4a3b39b96
Simon K. Bjørnøy <simon.bjornoy@gmail.com>
2021-03-10 19:55:02 +0100

    Updates

---
+++ precache-manifest.4cc7c77f1ee2ef467b311ccbc3c6aede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eca44f7f05c1e059592de28b3b511a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d97771b2c528eb8c8479",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "61ef65fd9535b904c3ec",
+    "url": "./static/js/2.9c3402d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9c3402d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97771b2c528eb8c8479",
+    "url": "./static/js/main.230e2e23.chunk.js"
+  },
+  {
+    "revision": "4f3b4d628e3923a1c66b",
+    "url": "./static/js/runtime-main.b1e89d5e.js"
+  }
+]);
